d80347480ebf432004fe78c16a87ba39047b747a
mattgimbal <matt.santamaria@gimbal.com>
2019-09-12 13:15:49 -0700

    nice

--- src/App.js
+++ src/App.js
@@ -1,26 +1,97 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Firebase from 'firebase';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Raindrops extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      command: 'default',
+      currentUrlIndex: 0,
+      urls: [
+        "https://i.giphy.com/media/it1EknihaJwqs/giphy.gif",
+        "https://i.giphy.com/media/26vUU4DxOJrXsJCBW/giphy.gif",
+        "https://i.giphy.com/media/KBm1L3QujrAT6/giphy.gif",
+        "https://i.giphy.com/media/n7vIRMqxCelTa/giphy.gif",
+        "https://i.giphy.com/media/qnPp6q4lpQRsk/giphy.gif",
+        "https://i.giphy.com/media/xT0BKwLPTNJvyyAoaQ/giphy.gif",
+        "https://i.giphy.com/media/1Sp9scsT89HZS/giphy.gif",
+        "https://i.giphy.com/media/FcVCVXpugGiFq/giphy.gif"
+      ]
+     };
+  }
+
+  componentDidMount() {
+    const db = Firebase.firestore();
+    const raindropsRef = db.collection(`/commands`);
+
+    raindropsRef.onSnapshot((querySnapshot) => {
+      let command = '';
+
+      querySnapshot.forEach((doc) => {
+        command = doc.data().command;
+      });
+
+      let currentUrlIndex;
+      if (command === 'previous') {
+        currentUrlIndex = this.state.currentUrlIndex - 1;
+        
+        if (currentUrlIndex < 0) {
+          currentUrlIndex = 0;
+        }
+      } else if (command === 'next') {
+        currentUrlIndex = this.state.currentUrlIndex + 1;
+
+        if (currentUrlIndex > 7) {
+          currentUrlIndex = 7;
+        }
+      }
+
+      this.setState({ command: command, currentUrlIndex: currentUrlIndex });
+    });
+  }
+
+  render() {
+    const command = this.state.command;
+
+    if (command !== 'default') {
+      let currentUrl;
+      currentUrl = this.state.urls[this.state.currentUrlIndex];
+
+      // if (command === 'previous') {
+
+      //   // return (
+      //   //   <div>
+      //   //     <p style={{ flex: 1 }}>{command}</p>
+      //   //     <img src={{currentUrl}} />
+      //   //   </div>
+      //   // );
+      // } else if (command === 'next') {
+      //   // return (
+      //   //   <div>
+      //   //     <p style={{ flex: 1 }}>{command}</p>
+      //   //     <img src={{currentUrl}} />
+      //   //   </div>
+      //   // );
+      // }
+
+      return (
+        <div>
+          <p style={{ flex: 1 }}>{command}</p>
+          <img src={currentUrl} />
+        </div>
+      );
+      
+      // return (
+      //   <div style={{ flex: 1 }}>{command}</div>
+      // );
+    }
+
+    return (
+      <p>
+        Raindrops
+      </p>
+    );
+  }
 }
 
-export default App;
+export default Raindrops;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
+import Firebase from 'firebase';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const config = {
+  apiKey: 'AIzaSyAbog8thHCc_2lhPwHcP050Hugi97uYo1o',
+  authDomain: 'rainbarrel-dev.firebaseapp.com',
+  databaseURL: 'https://rainbarrel-dev.firebaseio.com',
+  projectId: 'rainbarrel-dev',
+  storageBucket: 'rainbarrel-dev.appspot.com',
+  messagingSenderId: '9213028884'
+};
+
+Firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
